21a52c989e375c827f4316e5f22a1bcc3315b89c
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2023-11-24 14:47:25 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import puppeteer from "puppeteer";
+
+const getQuotes = async () => {
+  // Start a Puppeteer session with:
+  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto("https://britishskydiving.org/where-can-i-skydive/", {
+    waitUntil: "domcontentloaded",
+  });
+};
+
+// Start the scraping
+getQuotes();
