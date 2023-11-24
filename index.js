dbaee14e7a599437aeecff494a3475e3eb3a6458
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2023-11-24 21:08:49 +0000

    added code to open the iframe of the website to be scraped

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer";
 
-const getQuotes = async () => {
+const getSkydiveList = async () => {
   // Start a Puppeteer session with:
   // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
   // - no default viewport (`defaultViewport: null` - website page will in full width and height)
@@ -11,10 +11,45 @@ const getQuotes = async () => {
 
   const page = await browser.newPage();
 
-  await page.goto("https://britishskydiving.org/where-can-i-skydive/", {
+  //await page.goto("https://britishskydiving.org/where-can-i-skydive", {
+  await page.goto("https://webtools.britishskydiving.org/DirectorySearchResult.aspx?newsearch=1", {
     waitUntil: "domcontentloaded",
+    waitUntil: "networkidle0"
   });
+  
+  await page.waitForSelector('.BPAMap');
+
+  await page.screenshot({ path: "./screenshot.jpg", type: "jpeg", fullPage: true });
+
+
+  const list = await page.evaluate(() =>{
+
+    const map = document.querySelectorAll(".BPAItem");
+   // const listItem2 = document.querySelector(".BPAItem"); 
+   
+  //  const text = listItem2.querySelector(".OMDirectoryResultAddress").innerText;
+  //  const author = listItem2.querySelector(".BPAOrgName").innerText;
+
+   //const listItem = document.querySelector(".BPAOrgName");
+
+   //const text = listItem2.querySelector(".OMDirectoryResultAddress").innerText;
+   //const author = listItem.querySelector(".author").innerText;
+
+  //  const listItem3 = document.querySelector(".tb_text_wrap");
+
+   
+  //  //const text = listItem.querySelector(".OMDirectoryResultAddress").innerText;
+  //  //const author = listItem.querySelector(".author").innerText;
+
+   return { map};
+
+  });
+
+  console.log(list);
+ 
+
+await browser.close();
 };
 
 // Start the scraping
-getQuotes();
+getSkydiveList();
